d0635e78a3900d9930ecc5c93d6088009090047e
shchang <shchang@linkedin.com>
2021-10-08 12:23:59 -0700

    add cmd.txt

--- main.cpp
+++ main.cpp
@@ -7,27 +7,27 @@ using GET_FUNC = int (*)();
 using NOOP_FUNC = ADD_FUNC;
 
 int main() {
-    cout << "start" << endl;
-    auto handle_a = dlopen("./liba.so", RTLD_LAZY);
-    auto handle_b = dlopen("./libb.so", RTLD_LAZY);
-    auto handle_no = dlopen("./libno.so", RTLD_LAZY);
+  cout << "start" << endl;
+  auto handle_a = dlopen("./liba.so", RTLD_LAZY);
+  auto handle_b = dlopen("./libb.so", RTLD_LAZY);
+  auto handle_no = dlopen("./libno.so", RTLD_LAZY);
 
-    if (!handle_a || !handle_b || !handle_no) {
-        cout << "dlopen failed" << endl << flush;
-    }
+  if (!handle_a || !handle_b || !handle_no) {
+    cout << "dlopen failed" << endl << flush;
+    cout << dlerror() << endl;
+    return 42;
+  }
 
+  ADD_FUNC add = reinterpret_cast<ADD_FUNC>(dlsym(handle_a, "add"));
+  GET_FUNC get = reinterpret_cast<GET_FUNC>(dlsym(handle_b, "get"));
+  NOOP_FUNC noop = reinterpret_cast<NOOP_FUNC>(dlsym(handle_no, "noop"));
 
+  // cout << "dlsymed" << endl;
 
-    ADD_FUNC add = reinterpret_cast<ADD_FUNC>(dlsym(handle_a, "add"));
-    GET_FUNC get = reinterpret_cast<GET_FUNC>(dlsym(handle_b, "get"));
-    NOOP_FUNC noop = reinterpret_cast<NOOP_FUNC>(dlsym(handle_no, "noop"));
-
-    // cout << "dlsymed" << endl;
-
-    add();
-    // cout << "add" << endl;
-    cout << get() << endl << flush;
-    // cout << "get" << endl;
-    noop();
-    // cout << "noop" << endl;
+  add();
+  // cout << "add" << endl;
+  cout << get() << endl << flush;
+  // cout << "get" << endl;
+  noop();
+  // cout << "noop" << endl;
 }
